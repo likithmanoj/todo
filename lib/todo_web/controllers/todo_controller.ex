309f757a816b2398f2b23150a926491964723dd0
likithmanoj <likithmanojm@gmail.com>
2025-05-05 13:11:21 +0530

    contoller commit 2

--- lib/todo_web/controllers/todo_controller.ex
+++ lib/todo_web/controllers/todo_controller.ex
@@ -1,23 +1,34 @@
 defmodule TodoWeb.TodoController do
   use TodoWeb, :controller
+  alias Todo.Todos
 
   def index(conn, _params) do
-
+    todos = Todos.list_todos()
+    render(conn,"index.html",todos: todos)
   end
-  def show() do
-
-  end
-
-  def create() do
-
+  def show(conn, %{"id" => id}) do
+    todo = Todos.get_todo!(id)
+    render(conn, "show.html",todo: todo)
   end
 
-  @spec update() :: nil
-  def update() do
+  def create(conn, %{"todo" => todo_params}) do
+    case Todos.create_todo(todo_params) do
+      {:ok, todo} ->
+        redirect(conn, to: ~p"/todos/#{todo.id}")  # or render a success page
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def update(conn, %{"id"=> id},%{"todo" => todo_params}) do
+
 
   end
 
-  def delete()do
-
-  end
+  def delete(conn, %{"id"=> id}) do
+      todo = Todos.get_todo!(id)
+      Todos.delete_todo(todo)
+      redirect(conn, to: ~p"/todos")
+    end
 end
